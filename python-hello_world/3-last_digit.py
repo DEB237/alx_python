dbd5181ce8d4f28ea34a5b95c8810a2b6a35f887
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-07-18 18:03:29 +0100

    Python-Hello, World

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5" . format(number, last_digit))
 elif last_digit ==0 :
-    print("Last digit of {:d} is {:d} and 0" . format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is 0" . format(number, last_digit))
 elif last_digit < 6: #and int(last_digit)!= 0:(needed only if this line comes before the previous one)
     print("Last digit of {:d} is {:d} and is less than 6 and not 0" . format(number, last_digit))
 
