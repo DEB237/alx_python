7ee1ba85d1e1e74ba85308eff0217c47b5783a81
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-07-27 15:34:22 +0100

    Python-import & modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,10 @@
 def print_args(arguments):
     num_arguments = len(arguments)
-    if num_arguments == 0: 
-        singular_plural = "arguments." 
-    elif num_arguments == 1: 
-        singular_plural = "argument:" 
-    elif num_arguments > 1: 
+    if num_arguments == 0:
+        singular_plural = "arguments."
+    elif num_arguments == 1:
+        singular_plural = "argument:"
+    elif num_arguments > 1:
         singular_plural = "arguments:"
 
     print("{} {}".format(num_arguments, singular_plural))
@@ -12,4 +12,9 @@ def print_args(arguments):
     if num_arguments > 0:
         for i, arg in enumerate(arguments, start=1):
             print("{}: {}".format(i, arg))
+
+
+if __name__ == "__main__":
+    arguments = [1, 2, 3]  # Example list of arguments
+    print_args(arguments)
 
