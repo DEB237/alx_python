1c92d82ef9db1ec4033df7a158037d293725801f
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-07-27 14:57:05 +0100

    Python-import & modules

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+def print_args(arguments):
+    num_arguments = len(arguments)
+    if num_arguments == 0: 
+        singular_plural = "arguments." 
+    elif num_arguments == 1: 
+        singular_plural = "argument:" 
+    elif num_arguments > 1: 
+        singular_plural = "arguments:"
+
+    print("{} {}".format(num_arguments, singular_plural))
+
+    if num_arguments > 0:
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, arg))
+
---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,2 @@
+import variable_load_2
+print("a = {}" .format(variable_load_2.a))
