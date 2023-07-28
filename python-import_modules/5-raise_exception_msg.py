d656707baa1d3c2c5f20074b06916cc9d403648e
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-07-28 09:00:59 +0100

    Python-import & modules

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,10 +1,8 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
 
-raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
-
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
-    
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
