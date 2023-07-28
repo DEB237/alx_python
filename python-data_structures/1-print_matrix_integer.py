ff67a2503a5d610bdc4095a953fbc37ed99866f6
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-07-28 11:28:48 +0100

    Python data structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,13 @@
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
+    if len(matrix) == 0 or len(matrix[0]) == 0:
         print()
-    
-    for row in matrix:
-        for i, num in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(num))
-            else:
-                print("{:d}".format(num), end=" ")
+    else:
+        for row in matrix:
+            for i, num in enumerate(row):
+                if i == len(row) - 1:
+                    print("{:d}".format(num))
+                else:
+                    print("{:d}".format(num), end=" ")
   
+matrix = [[]]
+print_matrix_integer(matrix)
