899f16b315346d7d01be19a795a730906fcd86a7
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-21 22:33:30 +0100

    python inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -11,10 +11,10 @@ class BaseGeometry:
     pass
 
     @classmethod
-    def __dir__():
+    def __dir__(cls):
         """
         Returns a list of attributes for the class, excluding init_subclass.
         """
-        attributes = super().__dir__()
+        attributes = super().__dir__(cls)
         return [attr for attr in attributes if attr != 'init_subclass']
       
