65480f5ab86ff683d2d12d7d20f0240f009297ed
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-21 22:23:42 +0100

    python inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,16 +8,16 @@ class BaseGeometry:
     An empty class representing the BaseGeometry.
     """
 
-    def __init_subclass__(cls):
-        pass
+    pass
 
-    def area(self):
-        pass
-
-    @classmethod
+    '''@classmethod
     def __dir__(cls):
         """
         Returns a list of attributes for the class, excluding init_subclass.
         """
         attributes = super().__dir__()
-        return [attr for attr in attributes if attr != 'init_subclass']
+        return [attr for attr in attributes if attr != 'init_subclass']'''
+    
+bg = BaseGeometry()
+
+print(dir(BaseGeometry))    
