078e5dcb2a25e7414f596a394b5aaad2f0ff3fbc
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-08-22 06:52:48 +0100

    python inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,13 +8,31 @@ class BaseGeometry:
     An empty class representing the BaseGeometry.
     """
 
-    pass
+    def area(self):
+        """
+        Calculates the area of the geometry object.
+        Raises:
+            Exception: Always raised with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
+
+    def __str__(self):
+        """
+        Returns a string representation of the geometry object.
+        """
+        return "[BaseGeometry]"
+
+    def __repr__(self):
+        """
+        Returns a string representation of the object.
+        """
+        return "BaseGeometry()"
 
     @classmethod
     def __dir__(cls):
         """
         Returns a list of attributes for the class, excluding init_subclass.
         """
-        attributes = super().__dir__(cls)
+        attributes = super().__dir__()
         return [attr for attr in attributes if attr != 'init_subclass']
-      
+
