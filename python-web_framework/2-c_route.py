7642d7540dc7d910b970b2ab701dd78475905ccb
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-09-01 14:57:50 +0100

    Python web framework

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,31 @@
+"""
+This script starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Handler function for the '/' route. Returns a greeting message."""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Handler function for the '/hbnb' route. Returns 'HBNB'."""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Handler function for the '/c/<text>' route.
+    Displays 'C ' followed by the value of the 'text' variable.
+    Replace underscore (_) symbols with a space."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    """
+    Main entry point of the application. Starts the Flask web server."""
+    app.run(host='0.0.0.0', port=5000)
---
+++ python-web_framework/README.md
@@ -0,0 +1,3 @@
+# Python Web Frameworks
+
+Here I learn how to use frameworks in order to develop web applications.
