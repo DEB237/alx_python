28a9a328b48c5f77d37e20bc2708604033f3c260
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-09-16 11:57:46 +0100

    Python Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,6 +2,9 @@
 Module that defines the BaseGeometry class.
 """
 class BaseGeometry:
+    """
+    An empty class representing the BaseGeometry.
+    """
     def __init_subclass__(cls):
         """ Hide the init_subclass() method from the list of attributes
         and methods"""
