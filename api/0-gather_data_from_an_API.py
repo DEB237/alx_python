bea57bfe0e1c8e847aa6d65bc85b31eca948e0c3
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-10-10 17:36:59 +0100

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,7 @@
-"""
-This is a Module
-"""
+import csv
+from typing import List
 import requests
 import sys
-from typing import List
 
 
 def get_employee_info(employee_id: int) -> dict:
@@ -18,26 +16,30 @@ def get_todo_list(employee_id: int) -> List[dict]:
     return response.json()
 
 
-def display_todo_progress(employee_id: int) -> None:
+def export_to_csv(employee_id: int) -> None:
     employee_info = get_employee_info(employee_id)
     todo_list = get_todo_list(employee_id)
 
-    num_total_tasks = len(todo_list)
-    num_completed_tasks = sum(1 for task in todo_list if task['completed'])
-
     employee_name = employee_info.get('name', 'Unknown')
 
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{num_total_tasks}):")
+    file_name = f"{employee_id}.csv"
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    for task in todo_list:
-        if task['completed']:
-            print(f"\t{task['title']}")
+        for task in todo_list:
+            task_id = task['id']
+            task_title = task['title']
+            task_completed = str(task['completed'])
+            writer.writerow([employee_id, employee_name, task_completed, task_title])
+
+    print(f"Data exported to {file_name}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    display_todo_progress(employee_id)
+    export_to_csv(employee_id)
