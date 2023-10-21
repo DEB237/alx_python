97ad917f10279b5a9dee95722202a1c8cde5a423
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:44:44 +0100

    Python Inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,31 +1,81 @@
+
 """
-Module that defines the Rectangle class.
+Module containing the Rectangle class.
 """
+class OverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
 
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+    
 
-class Rectangle(BaseGeometry):
+class BaseGeometry(metaclass=OverrideMetaClass):
     """
-    Represents a rectangle.
+    This class models an empty class
     """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
+    def area(self):
+        """a method to raise an exception with a message"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        A method that validates value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
+class Rectangle(BaseGeometry, metaclass=OverrideMetaClass):
+    """
+    Models a rectangle. A derived class of BaseGeometgry
+    """
+    def __dir__(cls) -> None:
+        """
+        Removes __init_subclass__ from list of class attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
     def __init__(self, width, height):
         """
-        Initializes a rectangle object with the given width and height.
+        Call attriutes of parent.
+        validates attributes for positive integer
         """
-        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def __str__(self):
-        """
-        Returns a string representation of the rectangle.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+            """
+            Returns the string representation"""
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
     def area(self):
-        """
-        Calculates and returns the area of the rectangle.
-        """
-        return self.__width * self.__height
+            """
+            This method computes the area of the triangle
+            """
+            return self.__width * self.__height
