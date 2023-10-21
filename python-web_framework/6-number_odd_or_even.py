16709add9c0f2c4dfb43a5ea97c6b95c50aee232
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 15:13:31 +0100

    Python Web Frameworks

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,40 +1,121 @@
-"""
-This module implements a Flask web application
-with various routes and template rendering.
-"""
-from flask import Flask, render_template
+'''
+This python script starts a Flask web application and
+displays a text on the screen.
 
+function:
+    hello(): Displays a text on the screen
+        return - Text
+
+    hbnb(): Displays a text on the screen
+        return - Text
+
+    c(text): Displays a text on the screen
+        return - Text
+
+    python_(text=none): Displays a text on the screen
+        return - Text
+
+    numb(n): Chexks if n is integer
+        return - Text
+'''
+
+# Import the class Flask from flask module
+from flask import Flask, render_template
+from markupsafe import escape
+
+# Create an instance of Flask class
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!'
+    '''
+    A function that returns a text
+
+    return: a text
+    '''
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    '''
+    A function that returns a text
+
+    return: a text
+    '''
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    return f'C {text.replace("_", " ")}'
+def c(text):
+    '''
+    A function that returns a text
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+    return: a text
+    '''
+    text = text.replace('_', " ")
+    return "C {}".format(escape(text))
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    return f'Python {text.replace("_", " ")}'
+@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_(text=None):
+    '''
+    A function that returns a text
+
+    return: a text
+    '''
+    if text:
+        text = text.replace('_', " ")
+        return "Python {}".format(text)
+    else:
+        return "Python is cool"
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return f'{n} is a number'
+    '''
+    Checks if n is integer
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', number=n)
+    param:
+        n: value to be checked
+    return:
+        a text
+    '''
+    return "{} is a number".format(n)
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    even_odd = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', number=n, even_odd=even_odd)
+
+@app.route('/number_template/<int:n>')
+def display_html(n):
+    '''
+    Checks if n is integer
+
+    param:
+        n: value to be checked
+    return:
+        an html resource
+    '''
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def even_odd(n):
+    '''
+    Checks if n is integer, even or odd
+
+    param:
+        n: value to be checked
+    return:
+        an html resource
+    '''
+    if n % 2 == 0:
+        status = 'is even'
+    elif n % 2 != 0:
+        status = 'is odd'
+    return render_template('6-number_odd_or_even.html', n=n, status=status)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port='5000')
