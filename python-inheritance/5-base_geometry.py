e2f864decf9d3b54ac0c566761284331f4766964
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:39:19 +0100

    Python Inheritance

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,30 +1,46 @@
 """
-Module that defines a class
+This module is an empty class 
 """
-class BaseGeometry:
-    """
-    Base class for geometry shapes.
-    """
-    def area(self):
-        """
-        Placeholder method for calculating the area of a shape.
-        This method should be implemented by subclasses.
-        """
-        raise Exception("area() is not implemented")
 
+class OverrideMetaClass(type):
+    """
+    Override original inherited attributes from parent
+    """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+
+class BaseGeometry(metaclass=OverrideMetaClass):
+    """
+    This class models an empty class
+    """
+    def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
+    def area(self):
+        """a method to raise an exception with a message"""
+        raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         """
-        Validate that the given value is an integer greater than 0.
-        
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-        
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+        A method that validates value
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
