b01c23dcc3a1d9645568ba800814c7b1dda9cf1b
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:45:53 +0100

    Python Inheritance

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,20 +1,39 @@
-"""
-Module that defines the Square class.
-"""
 
-
+"""
+Module containing the Rectangle class.
+"""
 Rectangle = __import__('7-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    Represents a square.
+    A class or blueprint that models a square
     """
-
     def __init__(self, size):
         """
-        Initializes a square object with the given size.
+        instantiate attributes to an object
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+        Returns:
+            str: String representation of the Square instance.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
+    def area(self):
+        """
+        computes area of the square
+        """
+        return self.__size ** 2
