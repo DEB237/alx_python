6a40237eba2aba643dcbb84b09bbaf73e636cef4
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 13:15:22 +0100

    Python Network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,31 +1,13 @@
-'''
-This module contains a function
-'''
-
-import requests
+"""
+This script takes URL as argument and display the X-Request-Id
+"""
 import sys
+import requests
 
-def get_request_id(url):
-    """
-    Sends a request to the given URL and displays the value of the 'X-Request-Id' header.
+url = sys.argv[1]
+req = requests.get(url)
 
-    Args:
-        url (str): The URL to send the request to.
-
-    Returns:
-        None
-    """
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-
-    if request_id:
-        print(f"The value of the 'X-Request-Id' header is: {request_id}")
-    else:
-        print("The 'X-Request-Id' header is not present in the response.")
-
-# Check if a URL is provided as a command-line argument
-if len(sys.argv) > 1:
-    url = sys.argv[1]
-    get_request_id(url)
-else:
-    print("Please provide a URL as a command-line argument.")
+try:
+    print(req.headers['X-Request-Id'])
+except:
+    print("None")
