d58b5272b7e713ae3915f56277be6b10b00b6c15
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 15:19:21 +0100

    Python Inheritance

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,30 +1,8 @@
-"""
-Module that defines a class
-"""
-
+"""An empty class"""
 class BaseGeometry:
-    """
-    This class represents the base geometry.
-    """
-    def area(self):
-        """
-        Calculates the area of the geometry object.
-        Raises:
-            Exception: with the message "area() is not implemented".
-        """
-        def __dir__(cls) -> None:
-        """
-        control access to some inherited attributes
-        """
-        attributes = super().__dir__()
-        n_attributes = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                n_attributes.append(attr)
-        return n_attributes
+    """My BaseGeometry class"""
+    pass
     
     def area(self):
-        """
-        a method to raise an exception with a message
-        """
+        """function Area"""
         raise Exception("area() is not implemented")
