e753de067ecdcb8c6dc109a3a05f7ea65d6e29b4
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 13:16:42 +0100

    Python Network

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,33 +1,12 @@
-'''
-This module contains a function
-'''
-
-import requests
+"""
+ Python script that takes in a URL and an email address
+"""
 import sys
+import requests
 
-def post_email(url, email):
-    """
-    Sends a POST request to the given URL with the email as a parameter
-    and displays the body of the response.
+url = sys.argv[1]
+email = sys.argv[2]
+parameter = {'email':email}
 
-    Args:
-        url (str): The URL to send the POST request to.
-        email (str): The email address to include as a parameter in the request.
-
-    Returns:
-        None
-    """
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-
-    print(f"Your email is: {email}")
-    print("Response body:")
-    print(response.text)
-
-# Check if a URL and email address are provided as command-line arguments
-if len(sys.argv) > 2:
-    url = sys.argv[1]
-    email = sys.argv[2]
-    post_email(url, email)
-else:
-    print("Please provide a URL and an email address as command-line arguments.")
+response = requests.post(url, params=parameter)
+print("Email:",parameter['email'])
