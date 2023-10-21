71ed511fb09ce54ddd3f718792eaf819c200ddc9
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 15:23:50 +0100

    Python Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,4 @@
-"""
-Module containing the Rectangle class.
-"""
+""" Module containing the Rectangle class. """
 class OverrideMetaClass(type):
     """def __new__(cls, name, bases, attrs):
         # Customize the class creation process here
