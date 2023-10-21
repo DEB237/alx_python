44976bc3e5c2c8ec8d7d327234d6488d45d12e67
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:43:08 +0100

    Python Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,20 +1,44 @@
-"""
-Module that defines the Rectangle class.
-"""
 
+"""
+Module containing the Rectangle class.
+"""
+class OverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+    
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-    """
-    Represents a rectangle.
-    """
 
+class Rectangle(BaseGeometry, metaclass=OverrideMetaClass):
+    """
+    Models a rectangle. A derived class of BaseGeometgry
+    """
+    def __dir__(cls) -> None:
+        """
+        Removes __init_subclass__ from list of class attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
     def __init__(self, width, height):
         """
-        Initializes a rectangle object with the given width and height.
+        Call attriutes of parent.
+        validates attributes for positive integer
         """
-
-        self.integer_validator("width", width)
-        self.width = width
-        self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
