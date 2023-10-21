b5797688116b7b48b200c4ee41928f84d12ed1f2
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:25:36 +0100

    Python Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,15 @@
 """
 Module that defines the BaseGeometry class.
 """
-class BaseGeometry:
-    """
-    An empty class representing the BaseGeometry.
-    """
-   pass
+class OverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
