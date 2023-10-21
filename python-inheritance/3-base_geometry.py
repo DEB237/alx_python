42986178d85c400a0e870c85493b2425990175f9
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 15:18:15 +0100

    Python Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,16 +1,6 @@
-"""
-This module is an empty class 
-"""
-class OverrideMetaClass(type):
-    """def __new__(cls, name, bases, attrs):
-        # Customize the class creation process here
-        return super().__new__(cls, name, bases, attrs)"""
-
-    def __dir__(cls):
-        """
-        Returns:
-            list: List of attributes excluding __init_subclass__.
-        """
-        return [attribute for attribute in
-                super().__dir__() if attribute != '__init_subclass__']
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
+    
 
