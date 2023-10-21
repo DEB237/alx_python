83a46a0f6512d413fd9a6fb29c5ffd6211db07e1
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:23:55 +0100

    Python Inheritance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -17,4 +17,7 @@ def inherits_from(obj, a_class):
           inherited from the specified class,
           and False otherwise.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
