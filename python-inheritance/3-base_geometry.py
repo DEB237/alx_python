b83e47c4a39b28480cab35d895900facf09bd971
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 13:02:22 +0100

    Python Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,14 +1,10 @@
 """
 This module is an empty class 
 """
-
 class OverrideMetaClass(type):
-    """
-    Override original inherited attributes from parent
-    """
-    def __new__(cls, name, bases, attrs):
+    """def __new__(cls, name, bases, attrs):
         # Customize the class creation process here
-        return super().__new__(cls, name, bases, attrs)
+        return super().__new__(cls, name, bases, attrs)"""
 
     def __dir__(cls):
         """
@@ -17,3 +13,4 @@ class OverrideMetaClass(type):
         """
         return [attribute for attribute in
                 super().__dir__() if attribute != '__init_subclass__']
+
