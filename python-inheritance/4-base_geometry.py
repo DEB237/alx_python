4aec06b9217dd4bbae1c0b5b0318b6ca4f1db28d
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 12:29:55 +0100

    Python Inheritance

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -12,4 +12,19 @@ class BaseGeometry:
         Raises:
             Exception: with the message "area() is not implemented".
         """
+        def __dir__(cls) -> None:
+        """
+        control access to some inherited attributes
+        """
+        attributes = super().__dir__()
+        n_attributes = []
+        for attr in attributes:
+            if attr != '__init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+    
+    def area(self):
+        """
+        a method to raise an exception with a message
+        """
         raise Exception("area() is not implemented")
