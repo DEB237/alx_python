0b9908e10b52a55070051cd4f0059e45ff7e3c7a
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-21 15:27:55 +0100

    python-inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,40 +1,29 @@
-"""Module containing the Rectangle class."""
-class OverrideMetaClass(type):
-    """def __new__(cls, name, bases, attrs):
-        # Customize the class creation process here
-        return super().__new__(cls, name, bases, attrs)"""
-
-    def __dir__(cls):
-        """
-        Returns:
-            list: List of attributes excluding __init_subclass__.
-        """
-        return [attribute for attribute in
-                super().__dir__() if attribute != '__init_subclass__']
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
     
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+    def area(self):
+        """function Area"""
+        raise Exception("area() is not implemented")
 
-
-class Rectangle(BaseGeometry, metaclass=OverrideMetaClass):
-    """
-    Models a rectangle. A derived class of BaseGeometgry
-    """
-    def __dir__(cls) -> None:
-        """
-        Removes __init_subclass__ from list of class attributes
-        """
-        attributes = super().__dir__()
-        n_attributes = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                n_attributes.append(attr)
-        return n_attributes
-    
+    def integer_validator(self, name, value):
+        """function integer_validator"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+class Rectangle(BaseGeometry):
+    """A Rectangle subclass"""
     def __init__(self, width, height):
         """
-        Call attriutes of parent.
-        validates attributes for positive integer
+        Define the init method and inherits from the super.
+        Arguments:
+            width
+            height
         """
+        super()
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
