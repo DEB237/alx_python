9dcdb5edf328ca6f33026179eb4ceece85d7d35d
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-26 17:10:10 +0100

    python almost a circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,71 +1,39 @@
 '''
-This module contains a class
+This class implement the square method based and also inherit from the 
+Rectanngle class we previously built
 '''
 
+# from rectangle import Rectangle
 from models.rectangle import Rectangle
-'''
-Importing Base class from base
-'''
+# 
 
 class Square(Rectangle):
-    """
-    Square class inherits from Rectangle and represents a square shape.
-
-    Attributes:
-        id (int): The ID of the square.
-        size (int): The length of the sides of the square.
-        x (int): The x-coordinate position of the square.
-        y (int): The y-coordinate position of the square.
-    """
-
+    '''
+    this is a class that inherit from the rectangle class
+    '''
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The length of the sides of the square.
-            x (int, optional): The x-coordinate position of the square. Defaults to 0.
-            y (int, optional): The y-coordinate position of the square. Defaults to 0.
-            id (int, optional): The ID of the square. Defaults to None.
-
-        Notes:
-            Calls the super class with the necessary arguments to utilize the logic of the init in the Rectangle class.
-            Assigns each argument (size, x, y) to the corresponding attribute.
-        """
-        super().__init__(size, size, x, y, id)  # Call super() with size as both width and height
-        self.size = size
-
+        '''
+        instantation of the square class
+        '''
+        super().__init__(width=size,height=size, x=x,y=y, id=id)
+        
     def __str__(self):
-        """
-        Returns the string representation of the square.
-        Returns:
-            str: The string representation of the square.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        '''
+        This method convert the objects to strings
+        '''
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        """
-        Get the size (side length) of the square.
-        Returns:
-            int: The length of the sides of the square.
-        """
-        return self.width
+        '''
+        This method is defined to get te size of the object
+        '''
+        return super().Rectangle__width
 
     @size.setter
-    def size(self, value):
-        """
-        Set the size (side length) of the square.
-
-        Args:
-            value (int): The new length of the sides of the square.
-
-        Raises:
-            TypeError: If the value is not a positive integer.
-        """
-        if not isinstance(value, int) or value <= 0:
-           raise TypeError("Size must be a positive integer.")
-        
-
-        self.width = value
-        self.height = value
+    def size(self, size):
+        '''
+        THis is a setter method for size
+        '''
+        Rectangle.validation(self, 'width', size)
+        super().__init__(size, size)
