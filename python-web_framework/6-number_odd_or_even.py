5deb757f3a2ecaff8486ff9e80de843838d60387
Dounla Enricka Bernie <135113074+DEB237@users.noreply.github.com>
2023-10-30 21:37:41 +0100

    Update 6-number_odd_or_even.py

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,53 +1,49 @@
-# /usr/bin/python3
-"""
-a script that starts a Flask web application:
-"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    msg = "C {}".format(text.replace("_", " "))
-    return msg
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def py_text(text="is cool"):
-    msg = "Python {}".format(text.replace("_", " "))
-    return msg
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def int_only(n):
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def int_only_template(n):
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def even_or_odd(n):
-    if n % 2 == 0:
-        parity = "even"
-    else:
-        parity = "odd"
-    return render_template("6-number_odd_or_even.html", n=n, parity=parity)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+#!/usr/bin/python3 
+ """ Routes for all the tasks""" 
+ from flask import Flask, render_template 
+ app = Flask(__name__) 
+  
+  
+ @app.route('/', strict_slashes=False) 
+ def task_0(): 
+     return 'Hello HBNB!' 
+  
+  
+ @app.route('/hbnb', strict_slashes=False) 
+ def task_1(): 
+     return 'HBNB' 
+  
+  
+ @app.route('/c/<text>', strict_slashes=False) 
+ def task_2(text): 
+     word = text.split('_') 
+     return f"C {' '.join(word)}" 
+  
+  
+ @app.route('/python/', strict_slashes=False) 
+ @app.route('/python/<text>', strict_slashes=False) 
+ def task_3(text='is cool'): 
+     word = text.split('_') 
+  
+     return f"Python {' '.join(word)}" 
+  
+  
+ # Display if n is integer 
+ @app.route('/number/<int:n>', strict_slashes=False) 
+ def task_4(n): 
+     return f'{n} is a number' 
+  
+  
+ @app.route('/number_template/<int:n>', strict_slashes=False) 
+ def task_5(n): 
+     return render_template('5-number.html', num=n) 
+  
+  
+ @app.route('/number_odd_or_even/<int:n>', strict_slashes=False) 
+ def task_6(n): 
+  
+     return render_template('6-number_odd_or_even.html', num=n) 
+  
+  
+ if __name__ == "__main__": 
+     app.run(debug=True)
